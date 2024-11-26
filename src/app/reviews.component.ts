a19a64720486ff061ddb4097d0b856d9dfae7cbd
r2andrew <abradley4646@gmail.com>
2024-11-26 22:03:32 +0000

    add review form

--- src/app/reviews.component.ts
+++ src/app/reviews.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, ActivatedRoute } from '@angular/router';
 import { DataService } from './data.service';
-import {NgForOf} from '@angular/common';
+import { NgForOf, NgClass, NgIf } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'reviews',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NgForOf],
+  imports: [RouterOutlet, RouterModule, NgForOf, NgClass, NgIf, ReactiveFormsModule],
   providers: [DataService],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css'
@@ -14,12 +17,52 @@ import {NgForOf} from '@angular/common';
 export class ReviewsComponent {
   reviews_list: any;
   page: number = 1
-  constructor(public dataService: DataService) { }
+  reviewForm: any;
+
+  constructor(public dataService: DataService,
+              private formBuilder: FormBuilder,
+              private route: ActivatedRoute) {}
+
+  onSubmit() {
+    this.dataService.postReview(
+      this.route.snapshot.paramMap.get('id'),
+      this.reviewForm.value);
+    this.reviewForm.reset();
+  }
+
+  isUntouched() {
+    return this.reviewForm.controls.username.pristine ||
+      this.reviewForm.controls.title.pristine ||
+      this.reviewForm.controls.body.pristine ||
+      this.reviewForm.controls.file.pristine;
+  }
+  isIncomplete() {
+    return this.isInvalid('username') ||
+      this.isInvalid('title') ||
+      this.isInvalid('body') ||
+      this.isInvalid('file') ||
+      this.isUntouched();
+  }
+
+  isInvalid(control: any) {
+    return this.reviewForm.controls[control].invalid &&
+      this.reviewForm.controls[control].touched;
+  }
+
   ngOnInit() {
     if (sessionStorage['page']) {
       this.page = Number(sessionStorage['page']);
     }
     this.reviews_list = this.dataService.getReviews(this.page);
+
+    this.reviewForm = this.formBuilder.group( {
+      username: ['', Validators.required],
+      title:['', Validators.required],
+      body: ['', Validators.required],
+      rating: 5,
+      file: ['', Validators.required]
+    })
+
   }
   previousPage() {
     if (this.page > 1) {
