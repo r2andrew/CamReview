da54fb6531790069d537bfb3478ac1d8b0ceaba6
r2andrew <abradley4646@gmail.com>
2024-11-26 20:56:25 +0000

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReviewsComponent } from './reviews.component';
-import jsonData from '../assets/reviews.json';
+import { NavComponent } from './nav.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReviewsComponent],
+  imports: [RouterOutlet, ReviewsComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  ngOnInit() {
-    console.log(jsonData);
-  }
 }
