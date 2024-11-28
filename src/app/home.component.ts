ad069a5a253e7b217a5558eb2f7ec9e0feb657d9
r2andrew <abradley4646@gmail.com>
2024-11-28 16:06:29 +0000

    update homepage

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterModule} from '@angular/router';
+
 @Component({
   selector: 'home',
   standalone: true,
   templateUrl: './home.component.html',
+  imports: [
+    RouterModule,
+  ],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
